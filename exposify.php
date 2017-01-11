443ee7815f5274672739871b65134c3a6293d07c
Richard Keil <richartkeil@gmail.com>
2017-01-11 11:41:07 +0100

    fix: replace old version tag

--- exposify.php
+++ exposify.php
@@ -4,7 +4,7 @@
 Plugin Name: Exposify
 Plugin URI: https://exposify.de
 Description: Zeigt alle eigenen Immobilienangebote von Exposify.
-Version: 1.2.0
+Version: 1.2.1
 Author: Exposify
 Author URI: https://exposify.de
 License: GPL2
